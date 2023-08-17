6c962f025d0ccdef4a02de730b2281233999bc38
Ntpl007 <madhubabu.mandaloju@ntc-in.com>
2023-08-17 18:11:05 +0530

    Generated empty Transfer popup

---
+++ src/app/Components/PopUps/transfer-appointment/transfer-appointment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transfer-appointment',
+  templateUrl: './transfer-appointment.component.html',
+  styleUrls: ['./transfer-appointment.component.css']
+})
+export class TransferAppointmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
