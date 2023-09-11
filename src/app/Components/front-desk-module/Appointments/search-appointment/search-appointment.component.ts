fd1cd926af40cb7f9f432b747c8830032c29ec9a
Ntpl007 <madhubabu.mandaloju@ntc-in.com>
2023-09-11 13:24:52 +0530

    Recent changes

--- src/app/Components/front-desk-module/Appointments/search-appointment/search-appointment.component.ts
+++ src/app/Components/front-desk-module/Appointments/search-appointment/search-appointment.component.ts
@@ -126,12 +126,12 @@ isvi:boolean=false
   }
 
 
-  exportToExcel(): void {
-    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.patientList);
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-    XLSX.writeFile(wb, 'exported-data.xlsx');
-  }
+  // exportToExcel(): void {
+  //   const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.patientList);
+  //   const wb: XLSX.WorkBook = XLSX.utils.book_new();
+  //   XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+  //   XLSX.writeFile(wb, 'exported-data.xlsx');
+  // }
    onDateSelectfromdate(event:any):any {
     debugger
     let year = event.year;
